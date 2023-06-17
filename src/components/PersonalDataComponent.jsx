bd27832e34af7d1fcc8f679772c42a405471b755
afnaldi savaarizi <105402032+Afnaldi22@users.noreply.github.com>
2023-06-18 04:24:54 +0700

    Update PersonalDataComponent.jsx

--- src/components/PersonalDataComponent.jsx
+++ src/components/PersonalDataComponent.jsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react';
+import { database } from '../firebase.js';
 
+function Time() {
+  const [time, setTime] = useState('');
 
-const PersonalDataComponent = () => {
+  
+  useEffect(() => {
+    const fetchTime = async () => {
+      try {
+        const snapshot = await database.ref('/bus/waktu').once('value');
+        const waktu = snapshot.val();
+        setTime(waktu);
+      } catch (error) {
+        console.error('Gagal mengambil waktu:', error);
+      }
+    };
+    fetchTime();
+  }, []);
+
   return (
-    <div>PersonalDataComponent</div>
-  )
+    <div>
+      <h2>Time</h2>
+      <p>{time}</p>
+    </div>
+  );
 }
 
-export default PersonalDataComponent
+export default Time;
