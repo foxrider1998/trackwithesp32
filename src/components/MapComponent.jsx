dfe45ba9e984228692f1dd4274e2bf2d645c3ac8
afnaldi savaarizi <105402032+Afnaldi22@users.noreply.github.com>
2023-06-18 04:25:54 +0700

    Update MapComponent.jsx

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,9 +1,43 @@
+import { useState, useEffect } from 'react';
+import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
+import { database } from '../firebase';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+};
+
+const center = {
+  lat: 0,
+  lng: 0,
+};
+
+function Map() {
+  const [coordinates, setCoordinates] = useState(center);
 
 
-const MapComponent = () => {
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      try {
+        const snapshot = await database.ref('/bus').once('value');
+        const busData = snapshot.val();
+        const lat = busData.lat;
+        const lng = busData.long;
+        setCoordinates({ lat, lng });
+      } catch (error) {
+        console.error('Gagal mengambil koordinat:', error);
+      }
+    };
+    fetchCoordinates();
+  }, []);
+
   return (
-    <div>MapComponent</div>
-  )
+    <LoadScript googleMapsApiKey={YOUR_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap mapContainerStyle={containerStyle} center={coordinates} zoom={10}>
+        <Marker position={coordinates} />
+      </GoogleMap>
+    </LoadScript>
+  );
 }
 
-export default MapComponent
+export default Map;
